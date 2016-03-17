8fa2ccf2b67f33d1acaf7f7aff1e9291845884b9
Michael Rumler <m.rumler@gmx.de>
2016-03-17 19:12:36 +0100

    adjust version to main plugin version

--- version.php
+++ version.php
@@ -8,6 +8,6 @@
  * @author     Michael Rumler, rumler@ni.tu-berlin.de
  * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License
  */
-$version = 2016031600;
+$version = 2016031700;
 
 $release = 2.03;
